356cce9e049e1e24f81adc4d72cd2e7cd4faf3d3
gamagegit <cgamage92@yahoo.com>
2023-02-02 20:41:07 -0800

    202 solved

---
+++ 202_happy_no.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isHappy(self, n):
+        visit = set ()
+        while n not in visit:
+            visit.add(n)
+            n = self.sumOfSquares(n)
+            if n == 1:
+                return True
+        return False
+    
+    def sumOfSquares (self, n):
+            output = 0
+            while n:
+                digit = n % 10
+                digit = digit ** 2
+                output += digit
+                n = n // 10
+            return output    
+            
+    
+
+
+obj = Solution()
+print(obj.isHappy(19))
